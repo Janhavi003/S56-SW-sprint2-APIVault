f5f61335686fe345587a288402b86ee4315c4fbc
janhavi003 <janhavi.hivarekar@kalvium.community>
2026-09-16 13:51:48 +0530

    preserve project configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,4 +2,20 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-export default [{ignores:['dist']},{files:['**/*.{js,jsx}'],extends:[js.configs.recommended],languageOptions:{ecmaVersion:2020,globals:globals.browser,parserOptions:{ecmaVersion:'latest',ecmaFeatures:{jsx:true},sourceType:'module'}},plugins:{'react-hooks':reactHooks,'react-refresh':reactRefresh},rules:{...reactHooks.configs.recommended.rules,'react-refresh/only-export-components':['warn',{allowConstantExport:true}]}}]
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+])
