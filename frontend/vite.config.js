3193f566a64f2dfecfcc9eb20ad7fb288ef70522
janhavi003 <janhavi.hivarekar@kalvium.community>
2026-09-16 13:41:48 +0530

    implement APIVault first page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,3 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [react()] })
